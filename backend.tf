719290fc82524120879abb51986842b2600ae577
subh070 <subhasishyt99@gmail.com>
2026-04-08 07:09:28 +0000

    Update GitHub Actions for stage-based modular Terraform

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-batch4"
-    key            = "ec2/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
+    bucket       = "terraform-state-batch4"
+    key          = "ec2/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
